e227a6d2be675fb47e8096de69d1de63f83d5061
Philipp Hoehne <philipp.hoehne@stud.uni-goettingen.de>
2017-06-11 14:51:46 +0200

    first data loading

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
 import keras
+import numpy as np
+
+
+trainData = np.loadtxt("./data/train.csv", skiprows=1, delimiter=",")
+#Each line contains one image, 28x28=784 px. first Column is the number displayed.
+#	print(np.shape(trainData))
+#	(42000, 785)
+
