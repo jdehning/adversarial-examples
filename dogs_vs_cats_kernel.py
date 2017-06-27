f6ecdfdc74a92d4df3680efedf5227961575f3a8
Jonas Dehning <j.dehning@stud.uni-goettingen.de>
2017-06-27 16:56:03 +0200

    set correct bounds in generate_minimize

--- dogs_vs_cats_kernel.py
+++ dogs_vs_cats_kernel.py
@@ -17,18 +17,18 @@ from sklearn.model_selection import train_test_split
 from keras import backend as K
 import keras.layers.convolutional as conv
 
-ROWS = 64
-COLS = 64
+ROWS = 128
+COLS = 128
 CHANNELS = 3
 
 
-def open_data(beg = 0, end = None):
+def open_data_int(beg = 0, end = None):
     TRAIN_DIR = './data/dog_vs_cats/train/'
     train_dogs = [TRAIN_DIR + i for i in os.listdir(TRAIN_DIR) if 'dog' in i][beg:end]
     train_cats = [TRAIN_DIR + i for i in os.listdir(TRAIN_DIR) if 'cat' in i][beg:end]
     images = train_dogs + train_cats
     data_Y = np.array([1 for _ in range(len(train_dogs))] + [0 for _ in range(len(train_cats))])
-    data_X = prep_data(images) / 255.
+    data_X = prep_data_int(images)
     del images
     return data_X, data_Y
 
@@ -37,9 +37,9 @@ def read_image(file_path):
     return cv2.resize(img, (ROWS, COLS), interpolation=cv2.INTER_CUBIC)[...,::-1]
 
 
-def prep_data(images):
+def prep_data_int(images):
     count = len(images)
-    data = np.ndarray((count, ROWS, COLS, CHANNELS), dtype=np.uint8)
+    data = np.ndarray((count, ROWS, COLS, CHANNELS), dtype="uint8")
 
     for i, image_file in enumerate(images):
         image = read_image(image_file)
@@ -48,14 +48,26 @@ def prep_data(images):
 
     return data
 
-
+def shuffle_arr(arr1, arr2):
+    assert len(arr1) == len(arr2)
+    shuffle_index = np.random.choice(len(arr1), len(arr1), replace = False)
+    return arr1[shuffle_index], arr2[shuffle_index]
 
 #print("Images shape: {}".format(data_X.shape))
 
 nb_classes = 2
 
-data_X, data_Y = open_data(0, 11500)
-X_train, X_test, Y_train, Y_test = train_test_split(data_X, data_Y, test_size = 0)
+X_train, Y_train = open_data_int(-10000, None)
+X_train, Y_train = shuffle_arr(X_train, Y_train)
+X_validation, Y_validation = open_data_int(0, 2500)
+X_validation, Y_validation = shuffle_arr(X_validation, Y_validation)
+#X_train, X_test, Y_train, Y_test = train_test_split(data_X, data_Y, test_size = 0)
+"""
+for X, Y in zip(X_train, Y_train):
+    plt.imshow(X)
+    print(Y)
+    plt.show()
+"""
 #print(X_train[0])
 
 
@@ -75,6 +87,11 @@ def show_cats_and_dogs(idx):
 def catdog():
     model = Sequential()
 
+    #This 3 Layers for 128x128
+    model.add(Convolution2D(16, 3, 3, border_mode='same', input_shape=(ROWS, COLS, 3), activation='relu'))
+    # model.add(GaussianNoise(0.2))
+    model.add(Convolution2D(16, 3, 3, border_mode='same', activation='relu'))
+    model.add(MaxPooling2D(pool_size=(2, 2)))
 
     model.add(Convolution2D(32, 3, 3, border_mode='same', input_shape=(ROWS, COLS, 3), activation='relu'))
     #model.add(GaussianNoise(0.2))
@@ -116,7 +133,7 @@ def catdog():
     model.add(Dense(1, activation="sigmoid"))#, kernel_initializer='zeros'))
 
     model.compile(loss=keras.losses.binary_crossentropy,
-        optimizer = keras.optimizers.Adam(),#lr=0.001, decay=3e-4),
+        optimizer = keras.optimizers.Adam(lr=0.0001),#, decay=3e-4),
         metrics = ['accuracy'])
     model.summary()
     return model
@@ -168,8 +185,34 @@ def run_catdog():
     #print('Test accuracy:', score[1])
     return history
 
+steps_per_epoch_train = int(np.ceil(len(X_train)/batch_size))
+steps_per_epoch_val = int(np.ceil(len(X_validation)/batch_size))
+def generator_data(input, targets, num_steps):
+    step = 0
+    assert len(input) == len(targets)
+    len_data = len(input)
+    indices = None
+    input = input
+    targets = targets
+    num_steps = num_steps
 
-history = run_catdog()
+    while True:
+        if step == 0:
+            indices = np.random.choice(len_data, len_data, replace = False)
+        curr_indices = indices[step*batch_size:(step +1)*batch_size]
+        yield input[curr_indices].astype("float32")/255., targets[curr_indices]
+        step += 1
+        if step == num_steps:
+            step = 0
+            indices = None
+
+#history = run_catdog()
+history = LossHistory()
+model.fit_generator(generator=generator_data(X_train, Y_train, steps_per_epoch_train),
+                    steps_per_epoch=steps_per_epoch_train, epochs=nb_epoch, verbose=1, callbacks=[history],
+                    validation_data=generator_data(X_validation, Y_validation,steps_per_epoch_val),
+                    validation_steps=steps_per_epoch_val)
+
 
 model.save("keras_model_cat_dogs4")
 
@@ -185,7 +228,7 @@ plt.xticks(range(0,nb_epoch)[0::2])
 plt.legend()
 plt.show()
 
-test_X, test_Y = open_data(11500, None)
+#test_X, test_Y = open_data(11500, None)
 score = model.evaluate(test_X, test_Y, verbose=1)
 print('Test loss:', score[0])
 print('Test accuracy:', score[1])
--- minimize/generate_minimize_ex.py
+++ minimize/generate_minimize_ex.py
@@ -59,35 +59,6 @@ def read_data_mnist():
     data_Y = keras.utils.to_categorical(train[:, 0])
     return data_X, data_Y
 
-def minimize_func (r, c, image, target, loss_func):
-    """
-    the function that is going to be minimized\n
-    \n
-    r: (x, y, colors) array with the pertubation values\n
-    c: float factor that gets minimized through an additional line search\n
-    image: (x, y, colors) array with the image pixel values for all available colors\n
-    target: the target classification\n
-    loss_func: used loss function of the neuronal network. The function should expect\n
-    a pixel array (x, y, color) and a target and return an error. Needs to be a
-    continious function.\n
-    returns: value to be minimized
-    """
-    #print("d")
-    #return vec_abs(image + r)**2
-    return c * vec_abs(r) + loss_func(normalize_image(image + r), target)[0]
-
-def grad_min_func(r, c, image, target, model):
-    #print( np.shape(r))
-    #print(np.shape(get_gradient(
-    #	model, np.array([image + r]), np.array([target]))[0]))
-    #print(np.array([target]))
-    gradien = get_gradient(
-        model, np.array([normalize_image(image + r)]), np.array([target]))[0].astype("float64")
-    if(not np.any(gradien != 0)):
-        print("grad 0")
-    print(np.sum(np.abs(c * r / vec_abs(r))), np.sum(np.abs(gradien)))
-    return c * r / vec_abs(r) + gradien
-    #return gradien
 
 def vec_abs(arr):
     """
@@ -95,8 +66,6 @@ def vec_abs(arr):
     """
     return np.sqrt(np.sum(arr*arr))
 
-def normalize_image(image):
-    return image/np.max(image)
 
 def create_loss_func_for_minimize(model):
     """
@@ -117,61 +86,61 @@ def create_loss_func_for_minimize(model):
         return loss(prediction, target).eval(session=sess).astype("float64")
     return loss_func
 
-def run_batch_minimize(model, images, truePrediction):
+
+def run_minimizer(model, images, truePredictions):
     loss_func = create_loss_func_for_minimize(model)
-    i = 0
-    c = 1.
-    rs = []
+    c = 1
+    d = 1
     imgShape = np.shape(images[0])
-    def cb(a):
-        print ("a")
-    def to_minimize(r):
-        if (np.shape(r) != imgShape):
-            r = np.reshape(r, imgShape)
-        #print("c")
-        temp = minimize_func(r, c, image, np.append(truePrediction[i][shuffle:],truePrediction[i][:shuffle]), loss_func)
-        print(temp)
-        return temp
-    def grad(r):
-        shapeOld = np.shape(r)
-        if (np.shape(r) != imgShape):
-            r = np.reshape(r, imgShape)
-        return np.reshape(grad_min_func(r, c, image, np.append(truePrediction[i][shuffle:],truePrediction[i][:shuffle]), model), shapeOld)
 
-    #create bounds array
-    bounds = np.zeros((images[0].size, 2), dtype="float64")
-    bounds[:,1] = 1.0
+    for image, truePrediction in zip(images, truePredictions):
+
+        bounds = np.zeros((images[0].size, 2), dtype="float64")
+        bounds[:, 1] = 1.0
+        bounds[:,0] -= image.flatten()
+        bounds[:,1] -= image.flatten()
+        x0 = (np.random.rand(*imgShape)-image)*0.1
+
+        target_Y = np.zeros(10, dtype = "float32")
+        target_Y[3] = 1.
+        def to_minimize(r):
+            r = r.reshape(imgShape)
+            res_loss_func = d*loss_func(image + r, target_Y)[0]
+            res_norm  = c * vec_abs(r)
+            val = res_norm + res_loss_func
+            print("min: {:.3f}, {:.3f}, {:.3f}".format(val, res_loss_func, res_norm))
+            return val
+
+        def grad(r):
+            r = r.reshape(imgShape)
+            gradient = d*get_gradient(model, np.array([image + r]), np.array([target_Y]))[0].astype("float64")
+            #res_loss_func = d * loss_func(image + r, target_Y)[0]
+            #gradient = np.exp(res_loss_func) * grad
+            if np.sum(np.abs(gradient)) == 0:
+                print("grad 0")
+            norm_r = vec_abs(r)
+            grad_norm = c*r/norm_r
+            print("grad: {:.3f}, {:.3f}".format(np.max(np.abs(gradient)), np.max(np.abs(grad_norm))))
+            return (grad_norm + gradient).flatten()
+
+        tempR = scipy.optimize.minimize(to_minimize, jac=grad,
+                                        # x0=np.zeros(imgShape),
+                                        x0=x0, bounds=bounds,
+                                        method="L-BFGS-B", tol=0.001, options={"maxiter": 100, "eps": 0.01}).x.reshape(imgShape)
+        prediction_adv_ex = np.argmax(model.predict(np.array([image + tempR], dtype="float64"), batch_size=1, verbose=0))
+        prediction_im = np.argmax(truePrediction)
+        show_img_noise(image, tempR, predictImage=prediction_im, predictAdded=prediction_adv_ex)
 
 
-    curR = np.ones(imgShape)*10
-    for image in images:
-        for shuffle in np.arange(1, 10):
-            #print (to_minimize(np.array(np.random.rand(*imgShape), dtype="float32")))
-            #print(grad(np.array(np.random.rand(*imgShape)/10., dtype="float32")))
-            tempR = np.reshape(scipy.optimize.minimize(to_minimize, jac=grad, 
-               #x0=np.zeros(imgShape),
-               x0=np.random.rand(*imgShape)*5, bounds=bounds,
-               method="L-BFGS-B", callback=cb, tol=0.001, options={"maxiter": 20,
-               "eps":0.001} ).x, imgShape) 
-            prediction = np.argmax(model.predict(np.array([image + tempR], dtype="float64"), batch_size=1, verbose=0))
-            print(vec_abs(tempR) < vec_abs(curR))
-            print(prediction != np.argmax(truePrediction))
-            print(np.argmax(prediction))
-            print(np.argmax(truePrediction))
-            if (vec_abs(tempR) < vec_abs(curR) and prediction != np.argmax(truePrediction) ):
-                print("overWrite")
-                curR = tempR
-        rs.append(curR)
-        i += 1
-    return np.array(rs)
 
-model = load_model('../keras_model1')
-dataX, dataY = read_data_mnist()
-#print (dataY[7])
-#print(grad_min_func(dataX[3], 5, dataX[0], dataY[7], model))
-#rint (create_loss_func_for_minimize(model)(np.array(np.random.rand(28,28,1), dtype="float32"), dataY[1]))
-rs = run_batch_minimize(model, np.array([dataX[1]]), np.array([dataY[1]]))
-predicImg = np.argmax(model.predict(np.array([dataX[1]], dtype="float64"), batch_size=1, verbose=0))
-predicNoise = np.argmax(model.predict(np.array([rs[0]], dtype="float64"), batch_size=1, verbose=0))
-predicImgNoise = np.argmax(model.predict(np.array([dataX[1] + rs[0]], dtype="float64"), batch_size=1, verbose=0))
-show_img_noise(dataX[1], rs[0], predicImg, predicNoise, predicImgNoise)
+
+if __name__ == "__main__":
+    model = load_model('../keras_model1')
+    dataX, dataY = read_data_mnist()
+    rs = run_minimizer(model, np.array([dataX[0]]), np.array([dataY[0]]))
+    """
+    predicImg = np.argmax(model.predict(np.array([dataX[1]], dtype="float64"), batch_size=1, verbose=0))
+    predicNoise = np.argmax(model.predict(np.array([rs[0]], dtype="float64"), batch_size=1, verbose=0))
+    predicImgNoise = np.argmax(model.predict(np.array([dataX[1] + rs[0]], dtype="float64"), batch_size=1, verbose=0))
+    show_img_noise(dataX[1], rs[0], predicImg, predicNoise, predicImgNoise)
+    """
