a5ec0011e37c71c42205060ceca0960718af7454
Jonas Dehning <j.dehning@stud.uni-goettingen.de>
2017-07-12 16:02:14 +0200

    Updated figures minimizer

--- minimize/generate_minimize_ex.py
+++ minimize/generate_minimize_ex.py
@@ -207,7 +207,7 @@ def run_minimizer_inv(model, image, truePrediction, c = 1e2, plot=False, x0_fact
     inv_loss_func = create_inv_loss_func_for_minimize(model)
     c = c #put c = 1e3 for dogs vs cats
     d = 1
-    p = 2
+    p = 4
     imgShape = np.shape(image)
 
     bounds = np.zeros((image.size, 2), dtype="float64")
@@ -268,7 +268,7 @@ def run_minimizer_inv(model, image, truePrediction, c = 1e2, plot=False, x0_fact
             prediction_image = "cat" if prediction_image == 1 else "dog"
             prediction_adv_ex = "cat" if prediction_adv_ex == 1 else "dog"
         show_img_noise(image, noise, predictImage=prediction_image, predictAdded=prediction_adv_ex,
-                       save_as = save_as)
+                       save_as = save_as, std_noise = np.std(noise))
     return_dic = {"image_target": image_target,
                   "result_image": result_image,
                   "prediction_image": prediction_image,
@@ -288,8 +288,8 @@ if __name__ == "__main__":
     model = load_model("../keras_model_cat_dogs8")
     dataX, dataY = open_data_dogs_cat_float(end = 20, rows=128, cols=128)
     for i in range(0,1):
-        rs = run_minimizer_inv(model, dataX[i], dataY[i], plot=True,
-                               save_as="../figures/adv_example_minimizer_dogs_vs_cats.svg")
+        rs = run_minimizer_inv(model, dataX[i], dataY[i], plot=True, c = 1e3,
+                               save_as="../figures/adv_example_minimizer_dogs_vs_cats_test.svg")
     """
     predicImg = np.argmax(model.predict(np.array([dataX[1]], dtype="float64"), batch_size=1, verbose=0))
     predicNoise = np.argmax(model.predict(np.array([rs[0]], dtype="float64"), batch_size=1, verbose=0))
